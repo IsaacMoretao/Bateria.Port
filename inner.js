4d9b37dc14e90b72d27c910284c201d4c5d2664b
Isaac Moretão <isaachanam@gmail.com>
2022-01-25 17:56:25 -0300

    Commit

---
+++ inner.js
@@ -0,0 +1,49 @@
+function clicar1() {
+  var bumbo = window.document.getElementById('bumbo')
+  var espaco = window.document.getElementById('um')
+  
+  bumbo.play()
+
+}
+function clicar2() {
+  var surdo = window.document.getElementById('surdo')
+  var dois = window.document.getElementById('dois')
+  
+  surdo.play()
+
+}
+function clicar3() {
+  var ton1 = window.document.getElementById('ton1')
+  var tres = window.document.getElementById('tres')
+  
+  ton1.play()
+
+}
+function clicar4() {
+  var ton2 = window.document.getElementById('ton2')
+  var espaco = window.document.getElementById('um')
+  
+  ton2.play()
+
+}
+function clicar5() {
+  var caixa = window.document.getElementById('caixa')
+  var espaco = window.document.getElementById('um')
+  
+  caixa.play()
+
+}
+function clicar6() {
+  var chimball = window.document.getElementById('chimball')
+  var espaco = window.document.getElementById('um')
+  
+  chimball.play()
+
+}
+function clicar7() {
+  var prato = window.document.getElementById('prato')
+  var espaco = window.document.getElementById('um')
+  
+  prato.play()
+
+}
